b0302f09bd27fe6636f2a6e04f12b068e283a94b
yaoweiliu <18236968587@163.com>
2018-08-15 14:20:06 +0800

    quick_sort

---
+++ algorithm/quick_sort.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low;
+    int j;
+
+    for (j = low; j <= high- 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            swap(&arr[i], &arr[j]);
+            i++;
+        }
+    }
+    swap(&arr[i], &arr[high]);
+    
+    return i;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main(void)
+{
+    int arr[] = {3, 7, 8, 5, 2, 4};
+    quickSort(arr, 0, 5);
+    printf("Sorted array: \n");
+    printArray(arr, 6);
+    
+    return 0;
+}
