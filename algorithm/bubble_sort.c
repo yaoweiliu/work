d3c435081ff5dca74f74971040d4c091c9616906
yaoweiliu <18236968587@163.com>
2018-08-15 08:30:37 +0800

    second

--- algorithm/bubble_sort.c
+++ algorithm/bubble_sort.c
@@ -1,23 +1,37 @@
+/*数组作为函数的形参时会被退化为指针，此时不能用sizeof()来取得数组大小，只能直接传入数组大小*/
+
 #include <stdio.h>
+
+void bubble_sort(int len, int array[])
+{
+    int i, j, tmp;
+    int flag;
+
+    for(i = 0; i < len; i++) {
+        flag = 1;
+        for(j = 0; j < len - i - 1; j++) {
+            if(array[j] > array[j+1]) {
+                tmp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = tmp;
+                flag = 0;
+            }
+        }
+        if(flag)
+            break;
+    }
+
+    for(i = 0; i < len; i++)
+        printf("%d ", array[i]);
+    printf("\n");
+}
 
 int main(void)
 {
-    int i, j, tmp;
     int lt[] = {5,3,1,8,0,98,2};
+    int len = sizeof(lt)/sizeof(lt[0]);
 
-    for(i = 0; i < 7; i++) {
-        for(j = 0; j < 7 - i - 1; j++) {
-            if(lt[j + 1] > lt[j]) {
-                tmp = lt[j + 1];
-                lt[j + 1] = lt[j];
-                lt[j] = tmp;
-            }
-        }
-    }
-
-    for(i = 0; i < 7; i++)
-        printf("%d ", lt[i]);
-    printf("\n");
+    bubble_sort(len, lt);
     
     return 0;
 }
