e2ac9812744992261dca0dcef6b30e865f90a9b9
yaoweiliu <18236968587@163.com>
2018-11-10 19:39:13 +0800

    container_of

--- c_base/c_linux_kernel.c
+++ c_base/c_linux_kernel.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+#ifdef LINUX_KERNEL
 #define container_of(ptr, type, member) ({			\
 	const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
 	(type *)( (char *)__mptr - offsetof(type,member) );})
+#else
+#define container_of(ptr, type, member)	\
+	((type *)((char *)(ptr) - (unsigned long)(&((type *)0)->member)))
+#endif
 
 #define swap(a,b) \
 	do { typeof(a) __tmp = (a); (a) = (b); (b) = __tmp; } while(0)
