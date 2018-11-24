0f133d52873d1bfa774135ccf28252afe26dc86c
yaoweiliu <18236968587@163.com>
2018-11-24 14:49:33 +0800

    bit operations

--- c_base/bit.c
+++ c_base/bit.c
@@ -4,17 +4,19 @@ int main(int argc, char const *argv[])
 {
 	unsigned int data = 1024;
 	unsigned int val = 255;
+	unsigned int ret = 63;
 
 	printf("before data: %u\n", data);
 	printf("before val: %u\n", val);
+	printf("before ret: %u\n", ret);
 
 	data |= (1 << 6);
+	val &= ~(1 << 6);
+	ret |= (3 << 6);
 
 	printf("after data: %u\n", data);
-
-	val &= ~(1 << 6);
-
 	printf("after val: %u\n", val);
+	printf("after ret: %u\n", ret);
 
 	return 0;
 }
