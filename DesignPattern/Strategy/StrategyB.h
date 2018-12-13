a06b0c3147fb400b0f8e4a3ebb7a8e366e896ed4
yaoweiliu <18236968587@163.com>
2018-12-13 23:14:51 +0800

    strategy

---
+++ DesignPattern/Strategy/StrategyB.h
@@ -0,0 +1,15 @@
+#ifndef _STRATEGYB_H__
+#define _STRATEGYB_H__
+
+class StrategyB : public StrategyBase
+{
+public:
+	StrategyB() {};
+	~StrategyB() {};
+	
+	void algorithm(void);
+};
+
+
+#endif
+
---
+++ DesignPattern/Strategy/StrategyBase.h
@@ -0,0 +1,14 @@
+#ifndef _STRATEGYBASE_H__
+#define _STRATEGYBASE_H__
+
+class StrategyBase
+{
+public:
+	StrategyBase() {};
+	~StrategyBase() {};
+	
+	virtual void algorithm(void) = 0;
+};
+
+#endif
+
