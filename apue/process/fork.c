42f2d9778cd1e8d7bb94115635a4714c5ec5534f
yaoweiliu <18236968587@163.com>
2018-12-26 22:09:24 +0800

    fork

--- apue/process/fork.c
+++ apue/process/fork.c
@@ -2,10 +2,32 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 int main(int argc, char const *argv[])
 {
 	pid_t pid;
+	int i;
+
+#if 0
+	for(i = 0; i < 10; i++) {
+		fflush(NULL);
+		pid = fork();
+		if(pid < 0) {
+			perror("fork()");
+			exit(1);
+		}
+		else if(pid == 0) {
+			//TODO. dosomething use exec.
+			printf("[%d] chird.\n", getpid());
+			exit(0);
+		}
+	}
+
+	for(i = 0; i < 10; i++)
+		wait(NULL);
+
+#endif
 
 	fflush(NULL);
 
@@ -15,14 +37,15 @@ int main(int argc, char const *argv[])
 		exit(1);
 	}
 	else if(pid == 0) {
-		printf("[%d] chird.\n", getpid());
-		exit(0);
+		//execl("/bin/date", "date", "+%s", NULL);
+		execl("/bin/date", "date", NULL);
+		printf("no return.\n");
+		perror("execl()");
+		exit(1);
 	}
 	else {
-		printf("[%d] parent.\n", getpid());
+		wait(NULL);
 	}
-
-	sleep(5);
-
+	
 	return 0;
 }
