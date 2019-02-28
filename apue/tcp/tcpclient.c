e1ebbaa1ce9d1e8906fd0734879562b985fe59e2
yaoweiliu <18236968587@163.com>
2019-02-28 16:59:34 +0800

    tcp

--- apue/tcp/tcpclient.c
+++ apue/tcp/tcpclient.c
@@ -3,6 +3,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+ #include <sys/select.h>
+#include <sys/time.h>
+#include <sys/types.h>
 #include "tcpclient.h"
 
 static SOCKINFO *sinfo = NULL;
@@ -30,7 +33,10 @@ void destory_pipe(SOCKINFO *sinfo)
 
 int create_socket(SOCKINFO *sinfo, const char *ip, unsigned int port)
 {
-	struct sockaddr addr;
+	struct sockaddr_in dst_addr;
+    struct hostent *hostname;
+    int ret;
+    const char *buf = "create socket success."
 
 	sinfo->sfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sinfo->sfd == -1) {
@@ -38,7 +44,82 @@ int create_socket(SOCKINFO *sinfo, const char *ip, unsigned int port)
 		return -1;
 	}
 
+	hostname = gethostbyname(ip);
 
+    dst_addr.sin_family = AF_INET;
+    dst_addr.sin_port = htons(port);
+    dst_addr.sin_addr = *((struct in_addr *)hostname->h_addr);
+    memset(&(dst_addr.sin_zero), 0, sizeof(dst_addr.sin_zero));
+
+    ret = connect(sinfo->sfd, (struct sockaddr *)&dst_addr, sizeof(struct sockaddr));
+    if(ret) {
+    	perror("connect()");
+    	return -1
+    }
+
+    ret = send(sinfo->sfd, buf, strlen(buf), 0);
+    if(ret == -1) {
+    	perror("send()");
+    	return -1
+    }
+
+    return 0;
 }
 
-void destory_socket(SOCKINFO *sinfo);
+void destory_socket(SOCKINFO *sinfo)
+{
+	if(sinfo != NULL) {
+		close(sinfo->sfd);
+	}
+}
+
+void set_callback_func(SOCKINFO *sinfo, cb_func_t callback, unsigned int len)
+{
+	sinfo->cb = callback;
+}
+
+int select_handle(void)
+{
+	SOCKINFO *sinfo = NULL;
+	int ret, max_fd;
+	fd_set readfds;
+
+	sinfo = malloc(sizeof(SOCKINFO));
+	if(!sinfo) {
+		fprintf(stderr, "malloc error.");
+		return -1;
+	}
+
+	ret = create_socket(sinfo);
+	if(ret) {
+		fprintf(stderr, "create_socket error.");
+		return -1;
+	}
+
+	ret = create_pipe(sinfo);
+	if(ret) {
+		fprintf(stderr, "create_pipe error.");
+		return -1;
+	}
+
+	//ret = (sinfo->pipefd[0] > sinfo->pipefd[1]) ? sinfo->pipefd[0] : sinfo->pipefd[1];
+	max_fd = (sinfo->pipefd[0] > sinfo->sfd) ? sinfo->pipefd[0] : sinfo->sfd;
+
+	FD_ZERO(&readfds);
+
+	while(1) {
+		FD_SET(sinfo->sfd, &readfds);
+		FD_SET(sinfo->pipefd[0], &readfds);
+
+		select(max_fd+1, &readfds, NULL, NULL, NULL);
+		if(FD_ISSET(sinfo->sfd, &readfds)) {
+			//recv and recv callback. TODO
+		}
+
+		if(FD_ISSET(sinfo->pipefd[0], &readfds)) {
+			//read and send. TODO
+		}
+	}
+}
+
+
--- apue/tcp/tcpclient.h
+++ apue/tcp/tcpclient.h
@@ -1,10 +1,13 @@
 #ifndef __TCPCLIENT_H__
 #define __TCPCLIENT_H__
 
+typedef void (*cb_func_t)(void *buff, unsigned int len)
+
 typedef struct sock_fd
 {
 	int sfd;
 	int pipefd[2];
+	cb_func_t cb;
 }SOCKINFO;
 
 int create_pipe(SOCKINFO *sinfo);
@@ -14,6 +17,12 @@ void destory_pipe(SOCKINFO *sinfo);
 int create_socket(SOCKINFO *sinfo, const char *ip, unsigned int port);
 
 void destory_socket(SOCKINFO *sinfo);
+
+void set_callback_func(SOCKINFO *sinfo, cb_func_t callback, unsigned int len);
+
+int select_handle(void);
+
+
 
 #endif
 
