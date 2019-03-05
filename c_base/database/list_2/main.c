63bd08d71514198dfe06a471f70bbf44f211a0bd
yaoweiliu <18236968587@163.com>
2019-03-06 00:22:50 +0800

    list

--- c_base/database/list_2/main.c
+++ c_base/database/list_2/main.c
@@ -1,15 +1,32 @@
+/* https://github.com/BiscuitOS/HardStack/blob/master/Algorithem/list/single-list/TripPoint-Single.c */
+
 #include <stdio.h>
 
-struct data_base
+struct node
 {
 	int a;
-	struct data_base *next;
+	struct node *next;
 };
+
+void insert(struct node ***list, struct node *data)
+{
+
+}
 
 int main(int argc, char const *argv[])
 {
-	struct data_base data1, data2, data3, data4;
-	
+	struct node data1, data2, data3, data4;
+	struct node **data_list;
+	struct node *tmp;
+
+	data1.a = 0x1;
+	data2.a = 0x2;
+	data3.a = 0x3;
+	data4.a = 0x4;
+
+	data_list = &data1.next;
+
+	insert(&data_list, &data1);
 
 	return 0;
 }
