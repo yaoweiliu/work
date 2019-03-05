1ac4458df49f160a0d0be11f2bd444fa475a41a4
yaoweiliu <18236968587@163.com>
2019-03-06 00:14:44 +0800

    list

---
+++ c_base/database/list_2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct data_base
+{
+	int a;
+	struct data_base *next;
+};
+
+int main(int argc, char const *argv[])
+{
+	struct data_base data1, data2, data3, data4;
+	
+
+	return 0;
+}
