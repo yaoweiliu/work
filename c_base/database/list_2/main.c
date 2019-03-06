86f07c40194a0521bbb6026faba45b7c35071858
yaoweiliu <18236968587@163.com>
2019-03-06 23:35:50 +0800

    readme

--- c_base/database/list_2/main.c
+++ c_base/database/list_2/main.c
@@ -1,6 +1,7 @@
 /* https://github.com/BiscuitOS/HardStack/blob/master/Algorithem/list/single-list/TripPoint-Single.c */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 struct node
 {
@@ -10,7 +11,9 @@ struct node
 
 void insert(struct node ***list, struct node *data)
 {
-
+	**list = data;
+	*list = &data->next;
+	data->next = NULL;//需要设置data->next为NULL，否则37行for循环的第二个条件一直为真[野指针也有指向]
 }
 
 int main(int argc, char const *argv[])
@@ -26,7 +29,13 @@ int main(int argc, char const *argv[])
 
 	data_list = &data1.next;
 
-	insert(&data_list, &data1);
+	insert(&data_list, &data1);//改变二级指针的指向，需要使用三级指针
+	insert(&data_list, &data2);
+	insert(&data_list, &data3);
+	insert(&data_list, &data4);
+
+	for(tmp=&data1; tmp; tmp=tmp->next)
+		printf("data: %d\n", tmp->a);
 
 	return 0;
 }
