336afa2ffcbed5b51048a6ad0050a10d55b619a6
yaoweiliu <18236968587@163.com>
2019-06-04 15:09:53 +0800

    device_attr

--- device_attr/device_attr.c
+++ device_attr/device_attr.c
@@ -16,16 +16,18 @@ struct data_t
 
 static int __init attr_init(void)
 {
-	//register_chrdev(unsigned int major, const char *name, const struct file_operations *fops)
-	//class_create(owner, name)
-	//device_create(struct class *class, struct device *parent, dev_t devt, void *drvdata, const char *fmt, ...)
-	//sysfs_create_file(struct kobject *kobj, const struct attribute *attr)
+	//register_chrdev(unsigned int major, const char *name, const struct file_operations *fops);
+	//class_create(owner, name);
+	//device_create(struct class *class, struct device *parent, dev_t devt, void *drvdata, const char *fmt, ...);
+	//sysfs_create_file(struct kobject *kobj, const struct attribute *attr);
 	return 0;
 }
 
 static void __exit attr_exit(void)
 {
-
+	//device_destroy(struct class *class, dev_t devt);
+	//class_destroy(struct class *cls);
+	//unregister_chrdev(unsigned int major, const char *name);
 }
 
 module_init(attr_init);
