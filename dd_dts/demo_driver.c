1688ac939969dedf3bee9e7cbb050ef0126657fd
yaoweiliu <18236968587@163.com>
2019-07-15 00:29:36 +0800

    dts

--- dd_dts/demo_driver.c
+++ dd_dts/demo_driver.c
@@ -58,6 +58,9 @@ static __exit void demo_exit(void)
 static int dts_probe(struct platform_device *pdev)
 {
 	struct device_node *node = NULL;
+	struct device_node *child = NULL;
+	const __be32 *prop;
+	int len, count;
 
 	printk("%s: entence.\n", __func__);
 
@@ -65,6 +68,54 @@ static int dts_probe(struct platform_device *pdev)
 		if(node)
 			printk("Find %s\n", node->full_name);
 	}
+
+	node = pdev->dev.of_node;
+
+	prop = of_get_property(node, "BiscuitOS_name", &len);
+	if(prop) {
+		printk("%s: Property is BiscuitOS_name, len is %d\n", __func__, len);
+		printk("%s: String is %s\n", __func__, prop);
+	}
+
+	prop = of_get_property(node, "BiscuitOS_int", &len);
+	if(prop) {
+		int value;
+
+		value = be32_to_cpup(prop);
+		printk("%s: Property is BiscuitOS_int, len is %d\n", __func__, len);
+		printk("%s: Value is %#x\n", __func__, value);
+	}
+
+	prop = of_get_property(node, "BiscuitOS_mult", &len);
+	if(prop) {
+		unsigned long value;
+
+		printk("%s: Property is BiscuitOS_mult, len is %d\n", __func__, len);
+		value = of_read_ulong(prop, of_n_addr_cells(node));
+		printk("%s: #cell 0 is %#lx\n", __func__, value);
+
+		prop += of_n_addr_cells(node);
+		value = of_read_ulong(prop, of_n_addr_cells(node));
+		printk("%s: #cell 1 is %#lx\n", __func__, value);
+	}
+
+	prop = of_get_property(node, "BiscuitOS_leg", &len);
+	if(prop)
+		printk("%s: BiscuitOS_leg property exist, len is %d\n", __func__, len);
+	else
+		printk("%s: BiscuitOS_leg property is not exist\n", __func__);
+
+	count = of_get_child_count(node);
+	printk("%s: %s has %d children\n", __func__, node->name, count);
+
+	printk("%s: %s child:\n", __func__, node->name);
+	for_each_child_of_node(node, child)
+		printk(" \"%s\"\n", child->name);
+
+	printk("%s: %s available child:\n", __func__, node->name);
+	for_each_available_child_of_node(node, child)
+		printk(" \"%s\"\n", child->name);
+
 
 	return misc_register(&demo_device);
 }
@@ -77,7 +128,7 @@ static int dts_remove(struct platform_device *pdev)
 }
 
 static const struct of_device_id DTS_demo_of_match[] = {
-	{.compatible = "ilitek,ili9341",},
+	{.compatible = "DTS_demo, BiscuitOS",},
 	{ },
 };
 
