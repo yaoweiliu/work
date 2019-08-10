525aeb75d3a27dd45ba6f340807a9274e6fad675
yaoweiliu <18236968587@163.com>
2019-08-11 00:51:23 +0800

    client

--- apue/socket/client.py
+++ apue/socket/client.py
@@ -2,11 +2,15 @@
 #-*- coding:utf-8 -*-
 
 import socket
+import signal
+
+def sigpipe_handler(sig, frame):
+    pass
 
 if __name__=="__main__":
 	sd = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	sd.connect(('192.168.8.1', 8000))
-
+	signal.signal(signal.SIGPIPE, sigpipe_handler)
 	while True:
 		msg = raw_input("please enter: ")
 		if not msg:
