5f9ad5c4fd55d722a1f0f0ce4f13781e993d5c11
yaoweiliu <18236968587@163.com>
2019-08-16 00:43:38 +0800

    sem

--- apue/socket/client.c
+++ apue/socket/client.c
@@ -7,21 +7,36 @@
 #include<sys/socket.h>  
 #include<netinet/in.h>
 #include <arpa/inet.h>
+#include <signal.h>
+#include <semaphore.h>
   
-#define MAXLINE 4096  
-  
+#define MAXLINE 4096
+static sem_t sem; 
+
+void handler(int arg)
+{
+    int value;
+    sem_post(&sem);
+    printf("%s: receive sigterm,", __func__);
+    sem_getvalue(&sem, &value);
+    printf(" 0 value is %d\n", value);
+}
   
 int main(int argc, char** argv)  
 {  
     int sockfd, n,rec_len;  
     char recvline[4096], sendline[4096];  
     char buf[MAXLINE];  
-    struct sockaddr_in servaddr;  
+    struct sockaddr_in servaddr;
+    int value;
   
     if( argc != 2){  
         printf("usage: ./client <ipaddress><default 192.168.8.1>\n");  
         exit(0);  
-    }  
+    } 
+
+    sem_init(&sem, 0, 0);
+    signal(SIGINT, handler);
   
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){  
         printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);  
@@ -35,6 +50,9 @@ int main(int argc, char** argv)
         printf("inet_pton error for %s\n",argv[1]);  
         exit(0);  
     }  
+
+    sem_getvalue(&sem, &value);
+    printf("%s: 0 value is %d\n", __func__, value);
   
     if(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0){  
         printf("connect error: %s(errno: %d)\n",strerror(errno),errno);  
@@ -54,7 +72,16 @@ int main(int argc, char** argv)
     }  
     buf[rec_len]  = '\0';  
     printf("Received : %s\n",buf);  
-    close(sockfd);  
+    close(sockfd);
+
+    sem_getvalue(&sem, &value);
+    printf("%s: 1 value is %d\n", __func__, value);
+    sem_wait(&sem);
+    sem_getvalue(&sem, &value);
+     printf("%s: 2 value is %d\n", __func__, value);
+
+    sem_destroy(&sem);
+    
     exit(0);  
 }
 
