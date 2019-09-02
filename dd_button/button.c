570ee3b75c7d80b4813ed12dfcbb8e988c179f0a
yaoweiliu <18236968587@163.com>
2019-09-02 23:42:58 +0800

    button

--- dd_button/button.c
+++ dd_button/button.c
@@ -10,26 +10,55 @@
 #include <linux/slab.h>
 #include <linux/poll.h>
 #include <linux/wait.h>
+#include <linux/delay.h>
+#include <linux/list.h>
+#include <linux/spinlock.h>
+
+static struct list_head event_queue;
+static spinlock_t lock;
+
+struct button_data
+{
+	struct list_head head;
+	char to_user[0];
+};
 
 struct button_dev
 {
 	struct work_struct work;
 	struct struct workqueue_struct *workqueue;
-	int gpio;
+	int led_gpio;
+	int button_gpio;
 };
 
 static void work_func(struct work_struct *work)
 {
 	//turn on/off leds.
+	struct button_dev *button = container_of(work, struct button_dev, work);
+	gpio_set_value(button->led_gpio, 0);
+	msleep(10); //wq can sleep.
+	gpio_set_value(button->led_gpio, 1);// read datasheet.
 }
 
 static irqreturn_t button_interruppt(int irq, void *dev)
 {
 	//top half do nothing.
+	unsigned long flags;
 	struct button_dev *button = (struct button_dev *)dev;
 
 	INIT_WORK(&button->work, work_func);
 	queue_work(button->workqueue, button->work);
+
+	struct button_data *data = kzalloc(sizeof(struct button_data)+6, GFP_ATOMIC);
+	if(!data)
+		printk("%s: alloc memory error.\n", __func__);
+	data->to_user = "touser";
+
+	spin_lock_irqsave(&lock, flags);
+	list_add_tail(&data->head, &event_queue);
+	spin_unlock_irqrestore(&lock, flags);
+
+	wake_up_interruptible();
 
 	return IRQ_HANDLED;
 }
@@ -38,19 +67,24 @@ static int button_open(struct inode *inode, struct file *file)
 {
 	int err;
 
-	struct button_dev *button = kzalloc(sizeof(struct button_dev), GFP_KERNEL); //devm_kzalloc()
+	struct button_dev *button = kzalloc(sizeof(struct button_dev)+6, GFP_KERNEL); //devm_kzalloc()
 	if(!button)
 		goto alloc_error;
+	button->button_gpio = 30; //GPIO0_30
+	button->led_gpio = 39; //GPIO1_7
 
 	button->workqueue = create_singlethread_workqueue("button_wq");
 	if(!button->workqueue)
 		goto wq_error;
 	//1.read datasheet, get the button's gpio, then request gpio.
-	err = gpio_request_one(button->gpio, ); //devm_gpio_request_one();
+	err = gpio_request_one(button->button_gpio, ); //devm_gpio_request_one();
 	if(err)
-		goto gpio_error;
+		goto button_gpio_error;
+	err = gpio_request_one(button->led_gpio, );
+	if(err)
+		goto led_gpio_error;
 
-	err = request_irq(gpio_to_irq(button->gpio), button_interruppt, IRQF_TRIGGER_LOW, "button", button); //gpio_to_irq(), devm_request_irq()
+	err = request_irq(gpio_to_irq(button->button_gpio), button_interruppt, IRQF_TRIGGER_LOW, "button", button); //gpio_to_irq(), devm_request_irq()
 	if(err)
 		goto irq_error;
 
@@ -58,8 +92,11 @@ static int button_open(struct inode *inode, struct file *file)
 
 irq_error:
 	irq_free();
-	gpio_free(button->gpio);
-gpio_error:
+	gpio_free(button->button_gpio);
+	gpio_free(button->led_gpio);
+led_gpio_error:
+	gpio_free(button->button_gpio);
+button_gpio_error:
 	destroy_workqueue("button_wq");
 wq_error:
 	kfree(button);
@@ -74,7 +111,7 @@ static int button_close(struct inode *inode, struct file *file)
 
 static ssize_t button_read(struct file *file, char __user *buffer, size_t count, loff_t *offset)
 {
-
+	//get data from event_queue; and free memory.
 }
 
 static ssize_t button_write(struct file *file, const char __user *buffer, size_t count, loff_t *offset)
@@ -109,6 +146,8 @@ static struct miscdevice button_misc_device = {
 
 static int __init button_init(void)
 {
+	spin_lock_init(&lock);
+	INIT_LIST_HEAD(&event_queue);
 	return misc_register(&button_misc_device);
 }
 
